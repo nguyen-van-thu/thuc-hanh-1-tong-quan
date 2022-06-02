0af38571f72ddd44dd95297b56f2c38dbfd6a5ab
nguyen-van-thu <102147814+nguyen-van-thu@users.noreply.github.com>
2022-06-02 09:14:11 +0700

    thuc-hanh

---
+++ Newfolder/main.js
@@ -0,0 +1,11 @@
+const cars1 = ["AUDI","BMW","TATA","MERCEDES"];
+// Copied elements from cars1 to cars2 here
+const cars2 = [...cars1];
+
+// Copied elements from cars1 to cars3 here
+// and also add some new elements in cars3
+const cars3 = [...cars1,"NISSAN","TOYOTA"];
+
+console.log(cars1);
+console.log(cars2);
+console.log(cars3);
